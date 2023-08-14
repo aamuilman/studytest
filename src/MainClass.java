510a24c8c4505f334f6d9ec36b5bc04552157ca5
aamuilman <aamuilman@gmail.com>
2023-08-14 20:49:23 +0300

    add privet int

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,14 @@
 import org.junit.Test;
 
 public class MainClass  {
+   private int  class_number = 65;
 
 public int getLocalNumber()
 {
    return 14;
+}
+public int getClassNumber()
+{
+   return class_number;
 }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -13,4 +13,15 @@ public class MainClassTest extends MainClass
                     System.out.println("False");
                 }
     }
+    @Test
+    public void testGetClassNumber()
+    {
+     int a2 = this.getClassNumber();
+     if (a2>45){
+         System.out.println(a2);
+         }
+     else {
+         System.out.println("not equal to 45");
+     }
+    }
 }
